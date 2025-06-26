21b6cf688519eef73eb39cd07291142beed74179
kaila asus <mochzhafif1123@student.ub.ac.id>
2025-06-27 05:57:52 +0700

    refactor: delete instance checking in app factory since no databases is needed

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,25 +1,10 @@
-import os
-
 from flask import Flask
 from flask_cors import CORS
 
-def create_app(test_config=None):
+def create_app():
   # create and configure the app
-  app = Flask(__name__, instance_relative_config=True)
+  app = Flask(__name__)
   CORS(app=app)
-
-  if test_config is None:
-      # load the instance config, if it exists, when not testing
-      app.config.from_pyfile('config.py', silent=True)
-  else:
-      # load the test config if passed in
-      app.config.from_mapping(test_config)
-
-  # ensure the instance folder exists
-  try:
-      os.makedirs(app.instance_path)
-  except OSError:
-      pass
 
   # a simple endpoint that says hello
   @app.route('/')
